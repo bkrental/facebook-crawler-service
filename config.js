6db65726e57dba6668902054014a4cd1f9c18721
datdev2409 <datdev2409@gmail.com>
2023-10-10 22:09:35 +0700

    feat: initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    // USERNAME_SELECTOR: 'label[aria-label="Email address or phone number"]',
+    USERNAME_SELECTOR: 'input[name="email"]',
+    LOGIN_BTN_SELECTOR: '//span[contains(text(), "Log in")]',
+    POST_SELECTOR: '[data-ad-comet-preview="message"]',
+    USERNAME: "", // your facebook username
+    PASSWORD: "", // your facebook password
+};
---
+++ index.js
@@ -0,0 +1,67 @@
+import puppeteer from "puppeteer";
+import { scrollPageToBottom } from "puppeteer-autoscroll-down";
+import config from "./config.js";
+
+// Helper function here
+const getActiveElement = async (page) => {
+    const element = await page.evaluateHandle(() => document.activeElement);
+    return element;
+};
+
+const sleep = (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+async function getPostsFromPage(pageURL, numOfPosts = 10) {
+    // Main content here
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+
+    const context = browser.defaultBrowserContext();
+    context.overridePermissions(pageURL, ["notifications"]);
+
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    await page.goto(pageURL);
+
+    // Login
+    await page.click(config.USERNAME_SELECTOR);
+    await page.screenshot({ path: "3.png" });
+    const emailInput = await getActiveElement(page);
+    await emailInput.type(config.USERNAME);
+
+    await page.keyboard.press("Tab");
+    const passwordInput = await getActiveElement(page);
+    await passwordInput.type(config.PASSWORD);
+
+    await page.keyboard.press("Enter");
+    await page.waitForNavigation();
+
+    let posts = [];
+    while (posts.length < numOfPosts) {
+        posts = await page.$$eval(config.POST_SELECTOR, (posts) =>
+            Array.from(posts).map((post) => post.textContent)
+        );
+        await scrollPageToBottom(page, { size: 500, delay: 250 });
+        await page.evaluate(() => {
+            const btns = document.querySelectorAll('div[role="button"]');
+            Array.from(btns)
+                .filter((btn) => btn.textContent === "See more")
+                .forEach((btn) => btn.click());
+        });
+    }
+
+    await Bun.write("data.txt", posts.join("\n\n"));
+
+    await browser.close();
+}
+
+if (Bun.argv[2]) {
+    const pageURL = Bun.argv[2];
+    const numOfPosts = Bun.argv[3] * 1;
+    getPostsFromPage(pageURL, numOfPosts);
+} else {
+    console.log("ERROR: Please specify the page URL");
+}
