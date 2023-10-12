8d0906495e6db6a39dd83d6fe8c839eb7317ce80
Cong Dat <98537938+datdev2409@users.noreply.github.com>
2023-10-12 20:42:00 +0700

    feat: change runtime to nodejs (#2)

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export default {
-    // USERNAME_SELECTOR: 'label[aria-label="Email address or phone number"]',
-    USERNAME_SELECTOR: 'input[name="email"]',
+    USERNAME_SELECTOR: 'label[aria-label="Email address or phone number"]',
     LOGIN_BTN_SELECTOR: '//span[contains(text(), "Log in")]',
     POST_SELECTOR: '[data-ad-comet-preview="message"]',
     USERNAME: "", // your facebook username
